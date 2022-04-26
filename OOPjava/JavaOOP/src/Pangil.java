c8b04d849ac1d7cdb586267a502f1fafa75ba99c
rekidh <89791981+rekidh@users.noreply.github.com>
2022-04-26 21:10:49 +0700

    OPP java

---
+++ OOPjava/JavaOOP/src/Pangil.java
@@ -0,0 +1,11 @@
+public class Pangil {
+    public static void main(String[] args) {
+
+        var reki = new Person();
+        reki.alamat="jln,bagus";
+        reki.name="reki";
+        reki.umur=32;
+        reki.getInfo();
+
+    }
+}
---
+++ OOPjava/JavaOOP/src/Person.java
@@ -0,0 +1,11 @@
+public class Person {
+    String name;
+    String alamat;
+    int umur;
+
+     void getInfo(){
+        System.out.println("name : "+name);
+         System.out.println("alamat : "+alamat);
+        System.out.println("umur :"+umur);
+    }
+}
